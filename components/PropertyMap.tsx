86764287a827e7b9de128d3b377063b2f2973152
Yuan Wang <gityw2021mbp13@yuanagain.com>
2026-01-14 08:12:31 -0600

    Fix JSX syntax error in PropertyMap component

--- components/PropertyMap.tsx
+++ components/PropertyMap.tsx
@@ -226,7 +226,7 @@ export default function PropertyMap({ center, hospital, properties, loading }: P
           </div>
           <div className="flex items-center space-x-2">
             <div className="w-3 h-3 bg-red-500 rounded-full"></div>
-            <span>>25 min drive</span>
+            <span>&gt;25 min drive</span>
           </div>
         </div>
       </div>
